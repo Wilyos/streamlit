34d9afe90db1dc92a2639b9c45919adb32ec7380
wilyos <wilyos99@gmail.com>
2023-05-08 13:45:21 -0500

    leve mod

--- app.py
+++ app.py
@@ -2,3 +2,5 @@ import streamlit as st
 
 
 st.title("Pagina Principal")
+
+st.text('Esta pagina es un pequeño proyecto para mi clase, si deseas ver los flitros de las bases de datos abre el menu oprimiendo la flechade la parte superior')
